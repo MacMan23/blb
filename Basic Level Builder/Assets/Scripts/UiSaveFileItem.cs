e4b5cab517b17770b1037e31bfd9c52307fec056
Brenden Epp <brendenepp@gmail.com>
2024-12-09 02:11:57 -0800

    Added more comments to file item
    Button highlighting done properly by button
    
    - In the UiListView script we pass in the highlight/pressed colors and the script will replace the normal color with the alternating color. We also set the main image color to white in the prefab.
     - This way we are technically performing a color swap instead of a tint so the highlight colors look the same for every file.
    
    File button stays highlighted over info button
    
    - When the mouse is over the info button, the file button also stays highlighted.
    
    File button highlight works with pop up ui
    
    - If a UI appears over this button, we should be reacting properly and unselecting.

--- Basic Level Builder/Assets/Scripts/UiSaveFileItem.cs
+++ Basic Level Builder/Assets/Scripts/UiSaveFileItem.cs
@@ -1,78 +1,97 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UiSaveFileItem : MonoBehaviour
+public class UiSaveFileItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-  public TextMeshProUGUI m_Text;
-  public TextMeshProUGUI m_TimeStamp;
-  public GameObject m_InfoButton;
-  public Color m_AutosaveColor = Color.white;
-  public Color m_ManualSaveColor = Color.yellow;
+	public TextMeshProUGUI m_Text;
+	public TextMeshProUGUI m_TimeStamp;
+	public GameObject m_InfoButton;
+	public Color m_AutosaveColor = Color.white;
+	public Color m_ManualSaveColor = Color.yellow;
 
-  public float m_HiddenRectTransformWidth = 2.0f;
-  public float m_VisibleRectTransformWidth = 31.0f;
+	public float m_HiddenRectTransformWidth = 2.0f;
+	public float m_VisibleRectTransformWidth = 31.0f;
 
-  public Color m_HiglightedColor;
-  public Color m_PressedColor;
-  private Color m_NormalColor;
+	FileSystem m_FileSystem;
+	public string m_FullPath { get; private set; }
 
-  FileSystem m_FileSystem;
-  public string m_FullPath { get; private set; }
-
-  private bool m_IsMouseHovering = false;
+	private bool m_IsMouseHovering = false;
 
 
-  public void Setup(FileSystem fileSystem, string fullPath, string fileName)
-  {
-    m_FileSystem = fileSystem;
-    m_FullPath = fullPath;
-    m_Text.text = fileName;
+	public void Setup(FileSystem fileSystem, string fullPath, string fileName)
+	{
+		m_FileSystem = fileSystem;
+		m_FullPath = fullPath;
+		m_Text.text = fileName;
 
-    m_Text.color = fileName.StartsWith("Auto") ? m_AutosaveColor : m_ManualSaveColor;
-  }
+		m_Text.color = fileName.StartsWith("Auto") ? m_AutosaveColor : m_ManualSaveColor;
+	}
 
 
-  public void Load()
-  {
-    m_FileSystem.LoadFromFullPath(m_FullPath);
-  }
+	public void Load()
+	{
+		m_FileSystem.LoadFromFullPath(m_FullPath);
+	}
 
-  public void Update()
-  {
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		m_IsMouseHovering = true;
+		ShowInfoButton();
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+    // This check is nessesary because the file info button is ontop of this button, but only the top most button contains the mouse,
+    // IE, OnPointerExit will trigger if the mouse leaves the button, or enters the ui button
     bool hovering = RectTransformUtility.RectangleContainsScreenPoint((RectTransform)transform, Input.mousePosition);
+		
+		// If the mouse is still over the file button
+		if (hovering)
+		{
+			// Select the file button so the hilight stays on
+			EventSystem.current.SetSelectedGameObject(gameObject);
+			// Return without deselecting so the info button stays up
+			return;
+		}
 
-    // Check if mouse is inside the history item
-    if (!m_IsMouseHovering && hovering)
-    {
-      m_IsMouseHovering = true;
-      ShowInfoButton();
-      m_NormalColor = GetComponent<Image>().color;
-      GetComponent<Image>().color = m_HiglightedColor;
-    }
-    else if (m_IsMouseHovering && !hovering)
-    {
-      m_IsMouseHovering = false;
-      HideInfoButton();
-      GetComponent<Image>().color = m_NormalColor;
-    }
-  }
+		Deselect();
+	}
 
-  private void ShowInfoButton()
-  {
-    // Scale text boxes to left a bit to make room for the info button
-    m_Text.rectTransform.offsetMax = new Vector2(-m_VisibleRectTransformWidth, m_Text.rectTransform.offsetMax.y);
-    m_TimeStamp.rectTransform.offsetMax = new Vector2(-m_VisibleRectTransformWidth, m_TimeStamp.rectTransform.offsetMax.y);
-    // Show info button
-    m_InfoButton.SetActive(true);
-  }
+	public void Update()
+	{
+    bool hovering = RectTransformUtility.RectangleContainsScreenPoint((RectTransform)transform, Input.mousePosition);
+		
+		// If the mouse moved outside the file button, but we are still in the hovered state; unselect button
+		// This case is mainly for when the mouse was over the info button and just left
+		if (m_IsMouseHovering && !hovering)
+			Deselect();
+	}
 
-  private void HideInfoButton()
-  {
-    // Scale text boxes back to normal
-    m_Text.rectTransform.offsetMax = new Vector2(-m_HiddenRectTransformWidth, m_Text.rectTransform.offsetMax.y);
-    m_TimeStamp.rectTransform.offsetMax = new Vector2(-m_HiddenRectTransformWidth, m_TimeStamp.rectTransform.offsetMax.y);
-    // Hide info button
-    m_InfoButton.SetActive(false);
-  }
+	private void Deselect()
+	{
+		// Unselect the file button if it was previously selected
+		EventSystem.current.SetSelectedGameObject(null);
+		m_IsMouseHovering = false;
+		HideInfoButton();
+	}
+
+	private void ShowInfoButton()
+	{
+		// Scale text boxes to left a bit to make room for the info button
+		m_Text.rectTransform.offsetMax = new Vector2(-m_VisibleRectTransformWidth, m_Text.rectTransform.offsetMax.y);
+		m_TimeStamp.rectTransform.offsetMax = new Vector2(-m_VisibleRectTransformWidth, m_TimeStamp.rectTransform.offsetMax.y);
+		// Show info button
+		m_InfoButton.SetActive(true);
+	}
+
+	private void HideInfoButton()
+	{
+		// Scale text boxes back to normal
+		m_Text.rectTransform.offsetMax = new Vector2(-m_HiddenRectTransformWidth, m_Text.rectTransform.offsetMax.y);
+		m_TimeStamp.rectTransform.offsetMax = new Vector2(-m_HiddenRectTransformWidth, m_TimeStamp.rectTransform.offsetMax.y);
+		// Hide info button
+		m_InfoButton.SetActive(false);
+	}
 }
