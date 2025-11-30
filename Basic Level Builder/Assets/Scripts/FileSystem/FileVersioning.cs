8301f8cbc89c4dbceef2e6a341aedb95cd214a69
Brenden Epp <brendenepp@gmail.com>
2025-11-29 21:20:08 -0800

    Fixed lower res thumbnails in build
    For some reason in builds the thumbnail tiles were rendered at 2x2 instead of 4x4, but that was mainly because the texture itself was scaled dowm.
    Something something, disable mipmaps fixes it.

--- Basic Level Builder/Assets/Scripts/FileSystem/FileVersioning.cs
+++ Basic Level Builder/Assets/Scripts/FileSystem/FileVersioning.cs
@@ -546,7 +546,7 @@ public static class FileVersioning
   public static Sprite GetThumbnailSprite(LevelData levelData)
   {
     byte[] bytes = Convert.FromBase64String(levelData.m_Thumbnail);
-    Texture2D tex = new(0, 0) // No real reason for the width/height values in the constructor, they will be overwritten anyways in LoadImage
+    Texture2D tex = new(0, 0, TextureFormat.RGBA32, false) // No real reason for the width/height values in the constructor, they will be overwritten anyways in LoadImage
     {
       filterMode = FilterMode.Point
     };
